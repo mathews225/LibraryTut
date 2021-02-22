0eb54336c53288ea089b9e8021bdac077956e55f
mathews225 <petemathews14@gmail.com>
2021-02-22 14:13:27 -0500

    Math Library

--- LibraryTut/MathLib.cs
+++ LibraryTut/MathLib.cs
@@ -2,13 +2,39 @@
 
 namespace LibraryTut {
 	public class MathLib {
-		
+
 		// Right click Solution.
 		// Add > New Project... > .Net Core Console 
 		// Select the top level of the new project
 		// Right click > Add > New project refernce
 		// Change the name of the namespace 
-		public string About { get; set; } = "about MathLib";
+
+
+		public static int Add(int a, int b) {
+			return a + b;
+		}
+		public static int Subtract(int a, int b) {
+			return a - b;
+		}
+		public static int Multiply(int a, int b) {
+			return a * b;
+		}
+		public static int Divide(int a, int b) {
+			return a / b;
+		}
+		public static int Modulo(int a, int b) {
+			return a % b;
+		}
+		public static int Power(int a, int b) {
+			var x = (b == 0) ? 1 : a;
+			for (var idx = 1; idx < b; idx++) {
+				x *= a;
+			};
+			return x;
+		}
+
+
+
 
 	}
 }
--- TestMathLib/Program.cs
+++ TestMathLib/Program.cs
@@ -4,8 +4,22 @@ namespace LibraryTut {
 	class Program {
 		static void Main(string[] args) {
 
-			var mathLib = new MathLib();
-			Console.WriteLine($"About is {mathLib.About}");
+			var a = 12;
+			var b = 3;
+
+			var x1 = MathLib.Add(a,b);
+			Console.WriteLine($"{a} + {b} = {x1}");
+			var x2 = MathLib.Subtract(a,b);
+			Console.WriteLine($"{a} - {b} = {x2}");
+			var x3 = MathLib.Multiply(a,b);
+			Console.WriteLine($"{a} * {b} = {x3}");
+			var x4 = MathLib.Divide(a,b);
+			Console.WriteLine($"{a} / {b} = {x4}");
+			var x5 = MathLib.Modulo(a,b);
+			Console.WriteLine($"{a} % {b} = {x5}");
+			var x6 = MathLib.Power(a,b);
+			Console.WriteLine($"{a} ^ {b} = {x6}");
+
 		}
 	}
 }
